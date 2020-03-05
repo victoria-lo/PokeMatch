6260579a4935fef7c13b960437d3dd3851d7c6b2
VicLo <victoria2666@gmail.com>
2020-03-04 21:50:49 -0500

    Update HomeFragment.java

--- app/src/main/java/com/gameguildstudios/pokematch/ui/home/HomeFragment.java
+++ app/src/main/java/com/gameguildstudios/pokematch/ui/home/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.gameguildstudios.pokematch.ui.home;
 
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -33,31 +37,53 @@ public class HomeFragment extends Fragment {
     private TextView type5;
     private TextView type6;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    private Button btn;
+
+    private String url;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+        //url="https://pokeapi.co/api/v2/pokemon/" + poke(1,2,3,4,5,6).getText() +"/"
 
-        poke1 = getView().findViewById(R.id.input_home1);
-        poke2 = getView().findViewById(R.id.input_home2);
-        poke3 = getView().findViewById(R.id.input_home3);
-        poke4 = getView().findViewById(R.id.input_home4);
-        poke5 = getView().findViewById(R.id.input_home5);
-        poke6 = getView().findViewById(R.id.input_home6);
+        poke1 = root.findViewById(R.id.input_home1);
+        poke2 = root.findViewById(R.id.input_home2);
+        poke3 = root.findViewById(R.id.input_home3);
+        poke4 = root.findViewById(R.id.input_home4);
+        poke5 = root.findViewById(R.id.input_home5);
+        poke6 = root.findViewById(R.id.input_home6);
 
-        type1 = getView().findViewById(R.id.type_home1);
-        type2 = getView().findViewById(R.id.type_home2);
-        type3 = getView().findViewById(R.id.type_home3);
-        type4 = getView().findViewById(R.id.type_home4);
-        type5 = getView().findViewById(R.id.type_home5);
-        type6 = getView().findViewById(R.id.type_home6);
+        type1 = root.findViewById(R.id.type_home1);
+        type2 = root.findViewById(R.id.type_home2);
+        type3 = root.findViewById(R.id.type_home3);
+        type4 = root.findViewById(R.id.type_home4);
+        type5 = root.findViewById(R.id.type_home5);
+        type6 = root.findViewById(R.id.type_home6);
+
+        btn = root.findViewById(R.id.btn_home);
+
         return root;
     }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse("DONE");
+            }
+        });
+    }
+
+    private void jsonParse(String url){
+        Toast.makeText(getContext(), url,Toast.LENGTH_LONG).show();
+    }
+
 }
