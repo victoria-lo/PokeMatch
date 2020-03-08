888c5691d9c8c5d0e03e907fb4480961fe5c1394
VicLo <victoria2666@gmail.com>
2020-03-08 00:39:28 -0500

    added pie chart API

--- app/src/main/java/com/gameguildstudios/pokematch/ui/results/ResultsFragment.java
+++ app/src/main/java/com/gameguildstudios/pokematch/ui/results/ResultsFragment.java
@@ -12,41 +12,67 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.gameguildstudios.pokematch.R;
 import com.gameguildstudios.pokematch.SharedViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultsFragment extends Fragment {
 
-    private TextView poke1;
+    private TextView label1;
+    private AnyChartView anyChartView;
 
     private SharedViewModel viewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_results, container, false);
-        poke1 = root.findViewById(R.id.label_result1);
+        label1 = root.findViewById(R.id.label_1);
+
         return root;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        anyChartView = view.findViewById(R.id.anyChartView);
 
     }
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         viewModel = new ViewModelProvider(getActivity()).get(SharedViewModel.class);
         viewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(String s) {
-                poke1.setText(showTopWeakneses(s));
+                setupPieChart(s);
             }
         });
     }
 
-    private String showTopWeakneses(String s){
-        String s1 = s.replace("{","").replace("}","")
-                .replaceAll("[0-9]","").replaceAll("=","");
-        return s1;
+
+    private void setupPieChart(String s){
+        String s1 = s.replace("{","").replace("}","");
+        String[] arr = s1.split("=|,");
+        List<DataEntry> data = new ArrayList<>();
+
+        Pie pie = AnyChart.pie();
+        for(int i=0; i<arr.length-1;i+=2){
+            String type = arr[i];
+            Integer number = Integer.parseInt(arr[i+1]);
+            data.add(new ValueDataEntry(type, number));
+        }
+
+        pie.data(data);
+        pie.title("Weaknesses");
+        anyChartView.setChart(pie);
     }
 }
